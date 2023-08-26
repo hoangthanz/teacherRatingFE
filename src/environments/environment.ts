5463bec22c8d0f00677756f7558c9adef1126c2d
HoangThan <hoangnx.it4@vnpt.vn>
2023-08-26 15:49:02 +0700

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
+  production: true,
   version: '1.1.0.7',
   // domain: 'https://localhost:7206',
-  domain: 'http://demothpttnh.ddns.net:8055',
+  domain: 'http://demothpttnh.ddns.net:8000',
   defaultSchoolId: '5bd994a3-5ea2-4064-83b0-4cf06194ba8c',
 };
